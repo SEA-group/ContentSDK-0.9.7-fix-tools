aaef3ca5b1ce85e7f1760ab6489e2b3583194f98
AstreTunes <mebius.uranus@gmail.com>
2020-08-21 00:40:54 +0200

    Add main script

---
+++ Obsolete_node_remover.m
@@ -0,0 +1,172 @@
+%% Available MP nodes check ver.2020.08.20a by AstreTunes from SEA group
+% This script checks available MP nodes in the current game client, and
+% save them in a list
+
+% !!!!!! Matlab version r2016b or later is required (mandatory) !!!!!!
+
+% How to use:
+% - put this script and content.mat in res_mods/PnFMods/
+% - run this script
+
+clc
+clear
+fclose all;
+tic
+
+%% Parameter
+
+gameVersion = '0.9.7.0';
+
+%% Load available nodes list
+
+contentFileName = ['content_', gameVersion, '.mat'];
+
+if isempty(dir(['./', contentFileName]))
+    toc;
+    error([contentFileName, ' doesn''t exist']);
+end
+
+load(contentFileName);
+
+%% Prepare log file
+
+logFile=fopen('Obsolete_node_remover.log', 'w');
+
+%% Generate file list
+
+visualList = dir('**/*.visual');
+
+if isempty(visualList)
+    
+    toc;
+    error('No .visual file found.');
+    
+end
+
+%% Check 
+
+for indVisual = 1 : size(visualList, 1)
+    
+    currentFileName = [visualList(indVisual).folder, '\', visualList(indVisual).name];
+    
+    % back up original visual file
+    copyfile(currentFileName, [currentFileName, 'bak']);
+    disp(['checking ', currentFileName, ' ...']);
+    
+    currentFileBackup = fopen([currentFileName, 'bak'], 'rt');
+    currentFile = fopen(currentFileName, 'w');
+    lineBuffer = 0;
+    exitCycle = 0;
+    isInNode = 0;
+    
+    while exitCycle == 0
+    
+        lineBuffer = fgetl(currentFileBackup);
+%         lineSpaceless = lineBuffer;
+%         lineSpaceless(isspace(lineSpaceless))=[];
+        
+        if isInNode
+            
+            if contains(lineBuffer, 'MP_') % if the current node is a MP node
+                
+                % check the identifier
+                if strcmp(lineBuffer(strfind(lineBuffer, 'MP_')+4), 'M')    % misc node, xMnnn
+                    
+                    nodeIdentifier = lineBuffer((strfind(lineBuffer, 'MP_') + 3) : (strfind(lineBuffer, 'MP_') + 7));
+                    
+                elseif strcmp(lineBuffer(strfind(lineBuffer, 'MP_')+4), 'T')    % technical node, xTxnnnn
+                    
+                    nodeIdentifier = lineBuffer((strfind(lineBuffer, 'MP_') + 3) : (strfind(lineBuffer, 'MP_') + 9));
+                    
+                end
+                
+                if max(contains(nodesMat, nodeIdentifier)) % node is available
+                    
+                    % write lineCache and the current line and the following 7 lines
+                    fprintf(currentFile, '%s\r\n', lineCache);
+                    fprintf(currentFile, '%s\r\n', lineBuffer);
+                    lineBuffer = fgetl(currentFileBackup);  % to <transform>
+                    fprintf(currentFile, '%s\r\n', lineBuffer);
+                    lineBuffer = fgetl(currentFileBackup);  % to <row0>
+                    fprintf(currentFile, '%s\r\n', lineBuffer);
+                    lineBuffer = fgetl(currentFileBackup);  % to <row1>
+                    fprintf(currentFile, '%s\r\n', lineBuffer);
+                    lineBuffer = fgetl(currentFileBackup);  % to <row2>
+                    fprintf(currentFile, '%s\r\n', lineBuffer);
+                    lineBuffer = fgetl(currentFileBackup);  % to <row3>
+                    fprintf(currentFile, '%s\r\n', lineBuffer);
+                    lineBuffer = fgetl(currentFileBackup);  % to </transform>
+                    fprintf(currentFile, '%s\r\n', lineBuffer);
+                    lineBuffer = fgetl(currentFileBackup);  % to </node>
+                    fprintf(currentFile, '%s\r\n', lineBuffer);
+                    
+                else   % node is obsolete
+                    
+                    % skip the current node
+                    fgetl(currentFileBackup);  % to <transform>
+                    fgetl(currentFileBackup);  % to <row0>
+                    fgetl(currentFileBackup);  % to <row1>
+                    fgetl(currentFileBackup);  % to <row2>
+                    fgetl(currentFileBackup);  % to <row3>
+                    fgetl(currentFileBackup);  % to </transform>
+                    fgetl(currentFileBackup);  % to </node>
+                    
+                    % write log
+                    disp(['node ', nodeIdentifier, ' removed;']);
+                    fprintf(logFile, 'node %s is removed from %s \r\n', nodeIdentifier, currentFileName);
+                    
+                end
+                
+            elseif contains(lineBuffer, 'SP_')  % if the current node is a SP node
+                
+                % skip the current node
+                fgetl(currentFileBackup);  % to <transform>
+                fgetl(currentFileBackup);  % to <row0>
+                fgetl(currentFileBackup);  % to <row1>
+                fgetl(currentFileBackup);  % to <row2>
+                fgetl(currentFileBackup);  % to <row3>
+                fgetl(currentFileBackup);  % to </transform>
+                fgetl(currentFileBackup);  % to </node>
+                
+            else   % if it's neither SP nor MP node
+            
+                % write lineCache and the current line
+                fprintf(currentFile, '%s\r\n', lineCache);
+                fprintf(currentFile, '%s\r\n', lineBuffer);
+                
+            end
+            
+            isInNode = 0;
+            
+        elseif ~isempty(lineBuffer)
+            
+            if lineBuffer==-1
+                
+                exitCycle=1;
+                
+            elseif contains(lineBuffer, '<node>')   % if the current line starts a new node
+            
+                isInNode = 1;
+                lineCache = lineBuffer;
+                
+            else
+            
+                fprintf(currentFile, '%s\r\n', lineBuffer);
+            
+            end
+            
+        end        
+    
+    end
+    
+    fclose(currentFileBackup);
+    fclose(currentFile);
+    
+end
+
+%%
+
+disp('log written to Obsolete_node_remover.log; routine finished.')
+fclose all;
+toc
+
--- README.md
+++ README.md
@@ -5,4 +5,20 @@
 ![GitHub issues](https://img.shields.io/github/issues-raw/SEA-group/Obsolete-MP-nodes-remover)
 ![GitHub downloads](https://img.shields.io/github/downloads/SEA-group/Obsolete-MP-nodes-remover/total)
 
-(Not yet completed)
+By AstreTunes @ SEA group
+
+## How to use
+1. Put `Obsolete_node_remover.m` and `content.mat` in `[WoWS folder]/bin/[largest number]/res_mods/PnFMods/`
+2. Run `Obsolete_node_remover.m`
+3. Original *.visual* files are backed up and renamed to **.visualbak*
+
+### Attention
+1. **Matlab version must be r2016b or later**
+2. **The backup will be overwritten if you run the script twice**
+
+**[Download](https://github.com/SEA-group/Obsolete-MP-nodes-remover/archive/Script_for_0.9.7.0.zip)**
+
+## To generate content.mat after new game patch
+1. Download [wowsunpack.exe](https://forum.worldofwarships.eu/topic/113847-all-wows-unpack-tool-unpack-game-client-resources/) and put it in WoWS installation folder;
+2. Open cmd in the same folder, type `wowsunpack.exe -l -x bin\[build_number]\idx -p ..\..\..\res_packages -I content/gameplay/*/misc/* -I content/gameplay/*/technical/* -X *textures* -X *lod* > content.txt`; note that the build number changes according to game client version;
+3. Put `content.txt` beside `Available_MP_nodes_check.m`, **check the version number in line 18**, and run the latter. The path of this script does not matter.
