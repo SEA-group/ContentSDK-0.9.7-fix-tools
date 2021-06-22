b54ec4aced479112c89a094e4a076ca1b5ea7162
AstreTunes <mebius.uranus@gmail.com>
2021-06-22 14:52:23 +0200

    Add glass_material.fx support

--- SEA_fx_path_fix_py3.py
+++ SEA_fx_path_fix_py3.py
@@ -1,25 +1,21 @@
+#! /usr/bin/env python3
+# coding:utf-8
+
 import os
-import hashlib;
-import sys;
-import re
-import time
+import sys
 import importlib
-importlib.reload(sys);
+importlib.reload(sys)
 import warnings
 warnings.filterwarnings("ignore")
+
 def file_extension(path): 
   return os.path.splitext(path)[1]
 
 def listFiles(dirPath):
-
     fileList=[]
-
     for root,dirs,files in os.walk(dirPath):
-
         for fileObj in files:
-
             fileList.append(os.path.join(root,fileObj))
-
     return fileList
 
 def GetFileNameAndExt(filename):
@@ -30,11 +26,7 @@ def GetFileNameAndExt(filename):
 
 # sys.setdefaultencoding("utf-8");
 def main(fileDir):
-
-    regex = r'FUNC_SYS_ADD_ACCDETAIL'
-
     fileList = listFiles(fileDir)
-
     for fileObj in fileList:
         if file_extension(fileObj) == '.mfm':
             print(fileObj)
@@ -46,6 +38,7 @@ def main(fileDir):
                 f.write(line.replace('shaders/std_effects/ship_material.fx','shaders/materials/pbs/ship_material.fx')\
                     .replace('shaders/std_effects/ship_material_skinned.fx','shaders/materials/pbs/ship_material_skinned.fx')\
                     .replace('shaders/std_effects/wire_material.fx','shaders/materials/pbs/wire_material.fx')\
+                    .replace('shaders/std_effects/glass_material.fx','shaders/materials/pbs/glass_material.fx')\
                     .replace('shaders/std_effects/ship_nodamage_material.fx','shaders/materials/pbs/ship_nodamage_material.fx')) 
             f.close()
 
