3e33f7c095e1679b87b9b0bafbc7635364b3ada5
AstreTunes <mebius.uranus@gmail.com>
2021-06-22 14:52:08 +0200

    Cleaned codes

--- SEA_common_path_fix_py3.py
+++ SEA_common_path_fix_py3.py
@@ -1,25 +1,22 @@
+#! /usr/bin/env python3
+# coding:utf-8
+
 import os
-import hashlib
 import sys
 import re
-import time
 import importlib
 importlib.reload(sys)
 import warnings
 warnings.filterwarnings("ignore")
+
 def file_extension(path): 
   return os.path.splitext(path)[1]
 
 def listFiles(dirPath):
-
     fileList=[]
-
     for root,dirs,files in os.walk(dirPath):
-
         for fileObj in files:
-
             fileList.append(os.path.join(root,fileObj))
-
     return fileList
 
 def GetFileNameAndExt(filename):
@@ -28,16 +25,9 @@ def GetFileNameAndExt(filename):
     (shotname,extension) = os.path.splitext(tempfilename)
     return shotname
 
-# sys.setdefaultencoding("utf-8");
 def main(fileDir):
-
-    regex = r'FUNC_SYS_ADD_ACCDETAIL'
-
     fileList = listFiles(fileDir)
-
     p1 = r"(?<=content/gameplay/).+?(?=/textures/)"
-    pattern1 = re.compile(p1)
-
     for fileObj in fileList:
         if file_extension(fileObj) == '.mfm':
             print(fileObj)
